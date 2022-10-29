39761641437260436a12dcfb7398518f58cb6aaa
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-10-30 01:07:39 +0900

    feat: 메타태그 추가
      - 미리보기 이미지 추가
      - twitter 메타태그 추가

--- src/utils/metatag.ts
+++ src/utils/metatag.ts
@@ -1,8 +1,14 @@
 export const setMetaTags = (title: string, description: string) => {
   document
-    .querySelector('meta[name="og:title"]')
+    .querySelector('meta[property="og:title"]')
+    .setAttribute('content', title);
+  document
+    .querySelector('meta[property="twitter:title"]')
     .setAttribute('content', title);
   document
     .querySelector('meta[name="og:description"]')
+    .setAttribute('content', description);
+  document
+    .querySelector('meta[name="twitter:description"]')
     .setAttribute('content', description);
 };
