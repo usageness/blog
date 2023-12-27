c8decd7dd7173d2d44cb7451df7a91c0c53eaa0f
YongRae_Kim (Usage) <kyr9389@naver.com>
2023-12-28 00:24:54 +0900

    feat: 글 parsing 예외 처리

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -4,12 +4,47 @@ export const parseDocument = (id: number, rawString: string) => {
   const splitBySubTitle = splitByTitle[1].split('subTitle: ');
   const splitByDate = splitBySubTitle[1].split('date: ');
   const splitByPtag = splitByDate[1].split('</p>');
+  let content = documentData.slice(2).join('<hr>');
+
+  // 네비게이션 파싱
+  let lastTextIndex = 0;
+  const headTextArray = [];
+  while (true) {
+    const tagStartPosition = content.indexOf('<h', lastTextIndex);
+    if (tagStartPosition === -1) break;
+
+    const headTagDepth = Number(content[tagStartPosition + 2]);
+    const textStartPosition = content.indexOf('>', tagStartPosition + 4);
+    let textEndPosition = content.indexOf('</h', textStartPosition + 1);
+    let parsedHeadText = content.substring(
+      textStartPosition + 1,
+      textEndPosition,
+    );
+
+    // 따옴표 처리
+    parsedHeadText = parsedHeadText.replace(/&#39;/g, "'");
+    parsedHeadText = parsedHeadText.replace(/&quot;/g, '"');
+
+    const parsedId = parsedHeadText.replace(/ /g, '-');
+    headTextArray.push({ parsedHeadText, parsedId, depth: headTagDepth });
+
+    // h 태그 classname 처리
+    const emptyPosition = content.indexOf(' ', tagStartPosition);
+    content =
+      content.substring(0, emptyPosition) +
+      ' class="content-heading" ' +
+      content.substring(emptyPosition + 1);
+
+    textEndPosition = content.indexOf('</h', textStartPosition + 1);
+    lastTextIndex = textEndPosition + 1;
+  }
 
   return {
     id,
     title: splitBySubTitle[0],
     subTitle: splitByDate[0],
     date: splitByPtag[0],
-    content: documentData.slice(2).join('<hr>'),
+    content: content,
+    navigator: headTextArray,
   };
 };
